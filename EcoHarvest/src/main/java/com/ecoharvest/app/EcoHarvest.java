56c5c150caa1600f4ebb2ca2a0e510dd71a519ba
Poodle Moth <110733000+WhitePoodleMoth@users.noreply.github.com>
2023-05-19 08:51:52 -0700

    Project Creation
    Criação do projeto em Java usando NetBeans.

---
+++ EcoHarvest/src/main/java/com/ecoharvest/app/EcoHarvest.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ecoharvest.app;
+
+/**
+ *
+ * @author Unknown Account
+ */
+public class EcoHarvest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
